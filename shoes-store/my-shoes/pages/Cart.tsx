67c4e4ea5c29089552ada9f527fe6058b35ba24e
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2024-01-11 21:52:16 +0200

    i

--- shoes-store/my-shoes/pages/Cart.tsx
+++ shoes-store/my-shoes/pages/Cart.tsx
@@ -1,13 +1,45 @@
-import React from 'react'
-import { SafeAreaView, View, Text } from 'react-native'
+import { SafeAreaView, View, Text, Image } from 'react-native'
 import { globalStyle } from './style/Style'
+import { useEffect, useState } from 'react'
 
-const Cart = () => {
-    
+interface CartItem {
+  id: number;
+  name: string;
+  url: any;
+  price: number;
+}
+
+interface CartProps {
+  selectedItem: CartItem;
+}
+
+
+const Cart: React.FC<CartProps> = ({selectedItem}) => {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+   console.log(cartItems);
+  useEffect(() => {
+    if (selectedItem) {
+       setCartItems((prevItems) => [...prevItems, selectedItem]);
+    }
+  }, [selectedItem]);
+
+
+
   return (
     <SafeAreaView>
-    <View>
-   <Text style={globalStyle.compStyle}>Cart</Text>
+    <View style={globalStyle.card}>
+    
+      {cartItems.map((item, index) => (
+        <><Text key={index} style={globalStyle.cardText}> {item.name} </Text>
+        <Image 
+        key={index}
+        source={item.url}
+        style={globalStyle.imageStyle}
+        />
+          <Text 
+        style={globalStyle.cardText}>{item.price}</Text>
+        </>    
+      ))}
    </View>
   </SafeAreaView>
   )
--- shoes-store/my-shoes/pages/Women.tsx
+++ shoes-store/my-shoes/pages/Women.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Image , ScrollView, View, Text, TouchableOpacity } from 'react-native';
 import { globalStyle } from './style/Style';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
@@ -12,7 +12,7 @@ type ProfileScreenNavigationProp = NavigationProp<RootStackParamList, 'Cart'>;
 const Women = () => {
   const navigation = useNavigation<ProfileScreenNavigationProp>();
 
-  const womenShoes = [
+  const [womenShoes, setWomenShoes] = useState([
     {
      name:"Black_Heels", 
      url:require("../assets/women/black_akev.png"),
@@ -43,12 +43,13 @@ const Women = () => {
      url:require("../assets/women/sport_white.png"),
      price:"68$"
     },
-];
+]);
 
 
-const handleClick = () =>{
+const handleClick = (women) =>{
+  console.log(women)
   navigation.navigate('Cart')
-  
+ 
 
 }
 
@@ -61,7 +62,7 @@ return (
           <TouchableOpacity
           style={globalStyle.card}
           key={index}
-          onPress={()=> handleClick()}
+          onPress={()=> handleClick(women)}
           >
           <Text style={globalStyle.cardText}>{women.name}</Text>
         <Image 
